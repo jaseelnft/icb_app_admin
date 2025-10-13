1d496eb23420aac2156ab0836e3f0a66ef9aedaa
Jaseel <jaseel@Jaseels-iMac.local>
2025-10-13 12:03:29 +0400

    Ordinery Push

--- src/pages/app-settings.tsx
+++ src/pages/app-settings.tsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react";
 import { IC } from "../components/librery";
-import { getVerifideToken, updateVerifideToken } from "../services/settings";
+import {
+  getTwitterPosts,
+  getVerifideToken,
+  updateTwitterPosts,
+  updateVerifideToken,
+} from "../services/settings";
 import { showToast } from "../services/toast";
 
 export default function AppSettingsPage() {
   return (
-    <div className="h-full text-[#C7CCD2] p-8">
+    <div className="h-full text-[#C7CCD2] p-8 flex flex-col gap-4">
       {/* <svg
         xmlns="http://www.w3.org/2000/svg"
         height="24px"
@@ -17,6 +22,7 @@ export default function AppSettingsPage() {
       </svg>
       &nbsp; App Settings Page Under Development */}
       <VerifiedToken />
+      <TwitterSetUp />
     </div>
   );
 }
@@ -83,7 +89,7 @@ function VerifiedToken() {
           Easily configure and update withdrawal rules and app details.
         </div>
       </div>
-      <div className="px-6 py-10">
+      <div className="px-6 py-8">
         <div className="flex gap-4">
           {datas.map((it, k) => (
             <div
@@ -147,14 +153,158 @@ function VerifiedToken() {
         <div className="flex gap-2 mt-5">
           <div
             className={"btn1 w-46 text-center " + (busy ? "busybtn" : "")}
-            style={{ padding: 11 , fontSize: 15 }}
+            style={{ padding: 11, fontSize: 15 }}
             onClick={_onSave}
           >
             Save Changes
           </div>
           <div
             className={"btn2 w-46 text-center " + (busy ? "busybtn" : "")}
-            style={{ padding: 11 , fontSize: 15 }}
+            style={{ padding: 11, fontSize: 15 }}
+            onClick={_onCancel}
+          >
+            Cancel
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function TwitterSetUp() {
+  const [datas, setdatas] = useState([] as any[]);
+  const [busy, setbusy] = useState(false);
+  const [isAdd, setisAdd] = useState(false);
+  const [name, setname] = useState("");
+  const [id, setid] = useState("");
+
+  useEffect(() => {
+    _loadData();
+  }, []);
+
+  const _loadData = async () => {
+    setisAdd(true);
+    setdatas((await getTwitterPosts()) || []);
+    setisAdd(false);
+  };
+
+  const _onRemove = (i: number) => {
+    let d = [...datas];
+    d.splice(i, 1);
+    setdatas(d);
+  };
+
+  const _onAdd = (e: any) => {
+    e.preventDefault();
+    if (busy || name.trim() === "") return;
+    let d = [...datas];
+    d.push({ name: name.trim(), id: id.trim(), new: true });
+    setdatas(d);
+    setname("");
+    setid("");
+    setisAdd(false);
+    return false;
+  };
+
+  const _onSave = async () => {
+    if (busy) return;
+    setbusy(true);
+    setisAdd(false);
+    await updateTwitterPosts(datas)
+      .then(() => {
+        showToast("Twitter posts updated successfully");
+        _loadData();
+      })
+      .catch(() => {});
+    setbusy(false);
+  };
+
+  const _onCancel = () => {
+    if (busy) return;
+    _loadData();
+    setisAdd(false);
+  };
+
+  return (
+    <div className="border-[1.5px] border-[#16263B] rounded-[24px] overflow-hidden bg-[#011022]">
+      <div className="bg-[#010513] px-7 py-6">
+        <div className="font-[500] text-xl pb-1">Twitter Post</div>
+        <div className="text-[15px]">
+          Manage you witter post and fetch the reaction here
+        </div>
+      </div>
+      <div className="px-6 py-8">
+        <div className="flex gap-4">
+          {datas.map((it, k) => (
+            <div
+              key={k}
+              className="mb-4 border border-[#16263B] rounded-lg p-2 pl-3 text-sm flex gap-2 items-center font-[500] bg-[#1A2A45]"
+              style={{ background: it.new ? "transparent" : "" }}
+            >
+              <div>
+                {it.name}
+                <div className="text-xs opacity-80">{it.id}</div>
+              </div>
+              <img
+                src={IC.closeRed}
+                className="cursor-pointer w-5 bg-[#010513] rounded"
+                onClick={() => _onRemove(k)}
+              />
+            </div>
+          ))}
+        </div>
+        {!isAdd && (
+          <div
+            className="text-[#4F8FE1] font-[500] cursor-pointer p-1 my-2"
+            onClick={() => setisAdd(true)}
+          >
+            + Add Post
+          </div>
+        )}
+        {isAdd && (
+          <form
+            className="border border-[#16263B] rounded-lg p-4 w-232 flex gap-3"
+            onSubmit={_onAdd}
+          >
+            <input
+              placeholder="Post Name"
+              className="border border-[#16263B] rounded-lg py-2 px-4 w-50 bg-[#0F1626] text-sm"
+              onChange={(e) => setname(e.target.value)}
+              value={name}
+            />
+            <input
+              placeholder="Post Id"
+              className="border border-[#16263B] rounded-lg py-2 px-4 w-100 bg-[#0F1626] text-sm"
+              onChange={(e) => setid(e.target.value)}
+              value={id}
+            />
+            <button
+              className="btn1 w-42 text-center text-sm"
+              style={{ padding: 11, fontSize: 13 }}
+              type="submit"
+            >
+              Add Post
+            </button>
+            <div
+              className="btn2 w-42 text-center text-sm"
+              style={{ padding: 11, fontSize: 13 }}
+              onClick={() => setisAdd(false)}
+            >
+              Cancel
+            </div>
+          </form>
+        )}
+        <div className="flex gap-2 mt-5">
+          <div
+            className={"btn1 w-46 text-center " + (busy ? "busybtn" : "")}
+            style={{ padding: 11, fontSize: 15 }}
+            onClick={_onSave}
+          >
+            Save Changes
+          </div>
+          <div
+            className={"btn2 w-46 text-center " + (busy ? "busybtn" : "")}
+            style={{ padding: 11, fontSize: 15 }}
             onClick={_onCancel}
           >
             Cancel
--- src/services/settings.tsx
+++ src/services/settings.tsx
@@ -12,3 +12,20 @@ export async function updateVerifideToken(body_: any[]): Promise<any> {
   let res = await api.put("api/admin/settings/verified-tokens", body);
   return res.data;
 }
+
+//
+//
+
+export async function getTwitterPosts(): Promise<any> {
+  try {
+    let res = await api.get("api/admin/settings/twitter-posts");
+    return res.data;
+  } catch (error) {}
+}
+
+
+export async function updateTwitterPosts(body_: any[]): Promise<any> {
+  const body = body_.map(({ name, id }) => ({ name, id }));
+  let res = await api.put("api/admin/settings/twitter-posts", body);
+  return res.data;
+}
