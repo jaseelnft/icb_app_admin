4931dec30badd86ec5478d8f7aaa465a876ba2ec
Jaseel <jaseel@Jaseels-iMac.local>
2025-10-30 15:25:10 +0400

    Ordinery Push

--- src/widgets/tags.tsx
+++ src/widgets/tags.tsx
@@ -2,10 +2,10 @@ export function StatusTags({ status }: { status: string }) {
   let s1 =
     "bg-[#00B6761A] px-2 py-1 rounded-[4px] text-xs text-[#00B676] font-[600] flex items-center gap-1";
   let s2 = "w-2 h-2 bg-[#00B676] rounded-[4px] mt-[1px]";
-  if (status === "succes") {
+  if (status === "SUCCESS") {
     status = "Aproved";
   } else if (
-    status === "rejected" ||
+    status === "REJECTED" ||
     status === "CANCELLED" ||
     status === "ERROR"
   ) {
@@ -15,7 +15,7 @@ export function StatusTags({ status }: { status: string }) {
     s1 =
       "bg-[#DF3A451A] px-2 py-1 rounded-[4px] text-xs text-[#DF3A45] font-[600] flex items-center gap-1";
     s2 = "w-2 h-2 bg-[#DF3A45] rounded-[4px] mt-[1px]";
-  } else if (status === "pending" || status === "PENDING") {
+  } else if (status === "PENDING") {
     status = "Pending";
     s1 =
       "bg-[#F17F1B1A] px-2 py-1 rounded-[4px] text-xs text-[#F1941B] font-[600] flex items-center gap-1";
